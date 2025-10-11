cb79ebc94552651516fb8ec1753d55ca1a2f65f8
TariqueNayer <tariquenayer2476@gmail.com>
2025-10-11 21:18:58 +0530

    making tests for post app

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
+from .models import Post
 # Create your tests here.
+
+class PostTests(TestCase):
+
+	def setUp(self):
+		self.user = get_user_model().objects.create_user(
+			username='testuser',
+			email='test@gmail.com',
+			password='secter',
+			age=20,
+		)
+
+		self.post = Post.objects.create(
+			title='A good title',
+			body='Nice body content',
+			author=self.user,
+		)
+
+	def test_string_representation(self):
+		post = Post(title='A sample title')
+		self.assertEqual(str(post), post.title)
+
+	def test_get_absolute_url(self):
+		self.assertEqual(self.post.get_absolute_url(), '/posts/1/')
+
+	def test_post_content(self):
+		self.assertEqual(f'{self.post.title}', 'A good title')
+		self.assertEqual(f'{self.post.author}', 'testuser')
+		self.assertEqual(f'{self.post.body}', 'Nice body content')
+
+	def test_post_view(self):
+		response = self.client.get('/posts/1/')
+		no_response= self.client.get('/post/100000/')
+		self.assertEqual(response.status_code,200)
+		self.assertEqual(no_response.status_code,404)
+		self.assertContains(response,'A good title')
+		self.assertTemplateUsed(response,'post_detail.html')
+
+	
